softmax and weights fix

test.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 #loss functions
 def cross_entropy_loss(y_true, y_pred):
     samples_amount = y_true.shape[0]
-    #y_pred_clipped = np.clip(y_pred, 1e-9, 1 - 1e-9)
-    return -np.sum(y_true * np.log(y_pred)) / samples_amount
+    y_pred_clipped = np.clip(y_pred, 1e-9, 1 - 1e-9)
+    return -np.sum(y_true * np.log(y_pred_clipped)) / samples_amount
 
 def mean_squared_error(y_true, y_pred):
     return np.mean((y_true - y_pred) ** 2)
@@ -16,23 +16,23 @@ def mean_squared_error(y_true, y_pred):
 class Network:
     def __init__(self, layers, activations, loss_function="mse", seed=None):
         if seed:
-            np.random.seed(seed)  
+            np.random.seed(seed)
         
         self.num_layers = len(layers)
         self.weights = []
         self.biases = []
         self.activations = activations  
         self.loss_function = loss_function
-        
-        #reate weights and biases
+
+        #weights and biases
         for i in range(self.num_layers - 1):
-            self.weights.append(np.random.randn(layers[i], layers[i + 1]))
+            limit = np.sqrt(6 / (layers[i] + layers[i + 1])) 
+            self.weights.append(np.random.uniform(-limit, limit, (layers[i], layers[i + 1])))
             self.biases.append(np.zeros((1, layers[i + 1])))
 
         self.weight_error_history = []
         self.bias_error_history = []
 
-    #activation functions
     def apply_activation(self, z, activation):
         if activation == "sigmoid":
             return 1 / (1 + np.exp(-z))
@@ -41,9 +41,18 @@ class Network:
         elif activation == "linear":
             return z
         elif activation == "cube":
+            #print("Input to softmax (z):", z)
+            #print("Max value in z:", np.max(z))
+            #print("Min value in z:", np.min(z))
             return z**3
+        elif activation == "softmax":
+            #print("Input to softmax (z):", z)
+            #print("Max value in z:", np.max(z))
+            #print("Min value in z:", np.min(z))
+            z_stable = z - np.max(z)
+            exps = np.exp(z_stable)
+            return exps / (np.sum(exps) + 1e-9)
 
-    #derivatives
     def apply_activation_derivative(self, a, activation):
         if activation == "sigmoid":
             return a * (1 - a)
@@ -52,27 +61,28 @@ class Network:
         elif activation == "linear":
             return 1
         elif activation == "cube":
-            return 2*a**2
+            return 2 * a ** 2
 
-    #forward
+
     def forward(self, X):
         self.activations_values = [X]
         self.z_values = []
-        
+
         for i in range(self.num_layers - 1):
             z = np.dot(self.activations_values[-1], self.weights[i]) + self.biases[i]
             self.z_values.append(z)
             a = self.apply_activation(z, self.activations[i])
             self.activations_values.append(a)
-        
         return self.activations_values[-1]
 
-    #backward
     def backward(self, X, y, output, learning_rate):
         deltas = []
 
-        output_error = y - output  
-        output_delta = output_error * self.apply_activation_derivative(output, self.activations[-1])
+        if self.activations[-1] == "softmax" and self.loss_function == "cross_entropy":
+            output_delta =  y - output
+        else:
+            output_error = y - output
+            output_delta = output_error * self.apply_activation_derivative(output, self.activations[-1])
 
         deltas.append(output_delta)
         
@@ -95,15 +105,13 @@ class Network:
             #save the errors
             self.weight_error_history.append(np.linalg.norm(weight_update))
             self.bias_error_history.append(np.linalg.norm(bias_update))
-    
+
+
     def train(self, X, y, epochs, learning_rate, print_loss=True):
         for epoch in range(epochs):
-
             output = self.forward(X)
-            
             self.backward(X, y, output, learning_rate)
             
-            #loss at every 1000 epochs
             if epoch % 1000 == 0 and print_loss:
                 if self.loss_function == "cross_entropy":
                     loss = cross_entropy_loss(y, output)
@@ -111,7 +119,6 @@ class Network:
                     loss = mean_squared_error(y, output)
                 print(f'Epoch {epoch}, Loss: {loss}')
 
-    # Function to plot weight and bias error history
     def plot_error_history(self):
         weight_error_history = np.array(self.weight_error_history)
         bias_error_history = np.array(self.bias_error_history)
@@ -141,13 +148,17 @@ class Network:
         if(regression):
             return predictions
         else:    
-            return (predictions >= 0.5).astype(int)
+            predictions = self.forward(X)
+            return np.argmax(predictions)
+
     
     
 def calculate_accuracy(y_true, y_pred):
-    size = len(y_true[0])
-    correct_predictions = np.sum(y_true == y_pred)
-    accuracy = correct_predictions / len(y_true) * 100 / size
+    y_true_labels = np.argmax(y_true)
+    #print(y_true_labels)
+    #print(y_pred)
+    correct_predictions = np.sum(y_true_labels == y_pred)
+    accuracy = (correct_predictions / len(y_true)) * 100
     return accuracy
 
 
@@ -190,16 +201,12 @@ def perform_tests_simple(path):
     return
 
 def perform_tests_three_gauss(path):
-    #Accuracy for data.simple.test.100:  95.0 %
-    #Accuracy for data.simple.test.500:  94.91111111111111 %
-    #Accuracy for data.simple.test.1000:  94.93333333333334 %
-    #Accuracy for data.simple.test.10000:  94.92777777777779 %
-    layers = [2, 6, 3]
-    activations = ["linear", "sigmoid"]  #"relu", "sigmoid" or "linear"
-    learning_rate = 0.003
+    layers = [2, 8,  3]
+    activations = ["linear", "softmax"]
+    learning_rate = 0.0001
     epochs = 10000
-    seed = 42
-    loss_function = "cross_entropy"  #"cross_entropy" or "mse"
+    seed = 41
+    loss_function = "cross_entropy"
 
     numbers = [100, 500, 1000, 10000]
     for num in numbers:
@@ -214,16 +221,15 @@ def perform_tests_three_gauss(path):
         nn = Network(layers, activations, loss_function=loss_function, seed=seed)
         nn.train(X, y, epochs, learning_rate, False)
 
-        test_file_path = path + f"data.three_gauss.train.{num}.csv"
-
+        test_file_path = path + f"data.three_gauss.test.{num}.csv"
         data = pd.read_csv(test_file_path, delimiter=',', header=0)
         X = data[['x', 'y']].to_numpy()
         cls = data['cls'].to_numpy() - 1 
         y = np.eye(3)[cls]
+        
         predictions = nn.predict(X)
-    
-        print(f"Accuracy for data.simple.test.{num}: ", calculate_accuracy(y, predictions), "%")
-
+        print(f"Accuracy for data.three_gauss.test.{num}: ", calculate_accuracy(y, predictions), "%")
+        #nn.plot_error_history()
     return
 
 def perform_tests_activation(path):
@@ -249,12 +255,12 @@ def perform_tests_activation(path):
         y = data[['y']].to_numpy() 
 
         #standarization of data
-        mean_X = np.mean(X, axis=0)
-        std_X = np.std(X, axis=0)
+        mean_X = np.mean(X)
+        std_X = np.std(X)
         X_standardized = (X - mean_X) / std_X
         
-        mean_y = np.mean(y, axis=0)
-        std_y = np.std(y, axis=0)
+        mean_y = np.mean(y)
+        std_y = np.std(y)
         y_standardized = (y - mean_y) / std_y
 
         nn = Network(layers, activations, loss_function=loss_function, seed=seed)
@@ -280,8 +286,8 @@ def perform_tests_cube(path):
     #Mean Squared Error for data.regression.test.500: 50003.65932079104
     #Mean Squared Error for data.regression.test.1000: 52768.59193611867
     #Mean Squared Error for data.regression.test.10000: 50443.42037943883
-    layers = [1,1,1]  
-    activations = ["cube","linear"] 
+    layers = [1,4,1]  
+    activations = ["sigmoid","linear"] 
     #learning_rate = 0.01
     epochs = 10000
     seed = 42
@@ -298,12 +304,12 @@ def perform_tests_cube(path):
         y = data[['y']].to_numpy() 
 
         #standarization of data
-        mean_X = np.mean(X, axis=0)
-        std_X = np.std(X, axis=0)
+        mean_X = np.mean(X)
+        std_X = np.std(X)
         X_standardized = (X - mean_X) / std_X
         
-        mean_y = np.mean(y, axis=0)
-        std_y = np.std(y, axis=0)
+        mean_y = np.mean(y)
+        std_y = np.std(y)
         y_standardized = (y - mean_y) / std_y
 
         nn = Network(layers, activations, loss_function=loss_function, seed=seed)
@@ -321,6 +327,7 @@ def perform_tests_cube(path):
 
         mse = mean_squared_error(y_test, predictions)
         print(f"Mean Squared Error for data.regression.test.{num}: {mse}")
+        nn.plot_error_history()
 
     return
 
@@ -369,8 +376,8 @@ if __name__ == "__main__":
     #perform_tests_simple(folder_path)
     #perform_tests_three_gauss(folder_path)
     folder_path = 'S:/SN/projekt1/regression/'
-    perform_tests_activation(folder_path)
-    #perform_tests_cube(folder_path)
+    #perform_tests_activation(folder_path)
+    perform_tests_cube(folder_path)
 
 
 